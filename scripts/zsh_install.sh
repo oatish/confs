ce6cc4b4eacaf7c742ac4af1c52179392c8c7e24
shane stephenson <stephenson.shane.a@gmail.com>
2024-06-30 15:07:24 -0400

    zsh script

---
+++ scripts/zsh_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+echo "source $HOME/.config/zsh.stuff" >> ~/.zshrc
+echo "source $HOME/.config/sh.stuff" >> ~/.zshrc
